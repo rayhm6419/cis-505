2720178a8369a1ca983a1a325d02854b42488376
rayhm6419 <haoml6419@gmail.com>
2024-03-09 01:00:59 -0800

    First HomeWork

---
+++ Module_1/HelloWorldApp/LeiHelloWorld.java
@@ -0,0 +1,9 @@
+public class LeiHelloWorld {
+public static void main(String[] args) {
+    String FirstName = "Haoming";
+    String LastName = "Lei";
+    System.out.println( "Hello world from" +" " + FirstName + LastName);
+
+
+}
+}
