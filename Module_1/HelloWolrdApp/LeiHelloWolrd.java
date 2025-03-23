fbb9d1b7d4cd052083feed5306f49f393f34c815
Raymond <aoshine@ip-10-6-10-175.us-west-2.compute.internal>
2025-03-23 15:13:31 -0700

    Frist Assignment

---
+++ Module_1/HelloWolrdApp/LeiHelloWolrd.java
@@ -0,0 +1,10 @@
+package Module_1.HelloWolrdApp;
+
+public class LeiHelloWolrd {
+
+    public static void main(String[] args) {
+        String lastName = "Lei";
+        String fristName = "Haoming";
+        System.out.println("Hello World from " + fristName + " " + lastName);
+    }
+}
